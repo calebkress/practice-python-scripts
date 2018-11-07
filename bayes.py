fdad94a668436fca1c5d66235a31c461c697a209
Caleb Kress <calebkress@gmail.com>
2018-11-06 20:44:58 -0600

    changed raw inputs to normal inputs, got rid of unnecessary float decalaration

--- bayes.py
+++ bayes.py
@@ -1,7 +1,7 @@
 # Calculates P(A|B) from user input data
 
-a = float(raw_input("What is the probability of event A? "))
-b = float(raw_input("What is the probability of event B? "))
-b_given_a = float(raw_input("What is the probability of event B, given event A? "))
+a = input("What is the probability of event A? ")
+b = input("What is the probability of event B? ")
+b_given_a = input("What is the probability of event B, given event A? ")
 a_given_b = (b_given_a * a) / b
 print("The probability of event A, given event B, is " + str(a_given_b) + ".")
