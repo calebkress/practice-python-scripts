cd0f3efcb0050eb61eac8064f6cb1ab49d2855e1
Caleb Kress <calebkress@gmail.com>
2018-11-07 19:25:40 -0600

    updated binomial.py

--- binomial.py
+++ binomial.py
@@ -4,7 +4,7 @@ import math
 n = input("What is your n value? ")
 k = input("What is your k value? ")
 p = input("What is the probability that the event occurs? ")
-perm = math.factorial(n) / math.factorial(n - k)
-binomial = perm * (p ** k) * ((1 - p) ** (n - k))
+comb = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+binomial = comb * (p ** k) * ((1 - p) ** (n - k))
 
 print("There is a " + str(binomial) + " chance.")
