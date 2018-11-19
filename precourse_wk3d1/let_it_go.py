5e8e65ef9346bddba73d73336f00a0a5ff70c3c7
Caleb Kress <calebkress@gmail.com>
2018-11-19 10:35:46 -0600

    wrote let_it_go.py

---
+++ precourse_wk3d1/let_it_go.py
@@ -0,0 +1,8 @@
+def check_elsa_fan(fan):
+    if fan == True:
+        return 'I love Elsa! She\'s my favorite!'
+    else:
+        return 'Let It Go, it\'s not that great.'
+
+print(check_elsa_fan(True))
+print(check_elsa_fan(False))
