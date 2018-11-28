51f75cdd8cdce80176ca5eddaee486a292658296
Caleb Kress <calebkress@gmail.com>
2018-11-27 18:45:22 -0600

    refactor print_divisors.py to list comprehension style

--- precourse_wk3d1/print_divisors.py
+++ precourse_wk3d1/print_divisors.py
@@ -1,8 +1,5 @@
 # Prints all divisors of a user input number.
 def print_divisors(n = input('Find all divisors of: ')):
-    result = []
-    for i in range(n, 0, -1):
-        if n % i == 0:
-            result.append(i)
+    result = [i for i in range(n, 0, -1) if n % i == 0]
     return result
 print(print_divisors())
