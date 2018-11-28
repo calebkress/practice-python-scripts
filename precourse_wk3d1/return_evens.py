0ea81d5f08832faccde3661af3bf4a047e91c632
Caleb Kress <calebkress@gmail.com>
2018-11-27 18:46:48 -0600

    refactor return_evens.py to list comprehension style

--- precourse_wk3d1/return_evens.py
+++ precourse_wk3d1/return_evens.py
@@ -1,9 +1,6 @@
 # Takes a list of numbers and returns the even values.
 def return_evens(user_str = raw_input('Enter a list of numbers in the format "1, 2, 3, ...": ')):
     user_list = user_str.split(', ')
-    result = []
-    for item in user_list:
-        if int(item) % 2 == 0:
-            result.append(item)
+    result = [item for item in user_list if int(item) % 2 == 0]
     return result
 print(return_evens())
