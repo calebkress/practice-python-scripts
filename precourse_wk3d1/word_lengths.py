1021b58b2bac8ced2529c0504febd27ca9995844
Caleb Kress <calebkress@gmail.com>
2018-11-27 18:48:07 -0600

    refactor word_lengths.py to list comprehension style

--- precourse_wk3d1/word_lengths.py
+++ precourse_wk3d1/word_lengths.py
@@ -1,8 +1,6 @@
 # Takes a string and returns a list of each word length.
 def word_lengths(str = raw_input('Enter your string: '), delimiter = raw_input('Enter your delimiter: ')):
     str_list = str.split(delimiter)
-    result = []
-    for item in str_list:
-        result.append(len(item))
+    result = [len(item) for item in str_list]
     return result
 print(word_lengths())
