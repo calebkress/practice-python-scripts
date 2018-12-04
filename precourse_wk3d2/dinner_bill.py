932dba1c18b412379a5cccc248c625077b0039e3
Caleb Kress <calebkress@gmail.com>
2018-12-04 10:32:08 -0600

    finished writing dinner_bill.py

--- precourse_wk3d2/dinner_bill.py
+++ precourse_wk3d2/dinner_bill.py
@@ -1,8 +1,7 @@
 # Calculates the total of a dinner bill, given total, tax rate, and tip percentage.
 def dinner_bill(pretax = input('Enter total pre-tax: '), taxrate = input('Enter the tax rate: '), tip_pct = input('Enter your tip percentage: ')):
     posttax = pretax + (pretax * taxrate)
-    tip_amt = posttax * (tip_pct / 100)
-    print(tip_amt)
+    tip_amt = posttax * tip_pct
     total = posttax + tip_amt
     return('Total after tax: ' + str(posttax) + ', Total with tip: ' + str(total))
 print(dinner_bill())
